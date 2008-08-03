af48b340d95b28ac5d2dc1121fa7540b4cad7b1c
anders.dahnielson <anders.dahnielson@2299fe86-fe49-0410-b9be-31dadaebf420>
2008-08-03 18:32:20 +0000

    Refactored demo4 voice into its own header file to be shared between demos.

--- sandbox/jack_client/demo4.cpp
+++ sandbox/jack_client/demo4.cpp
@@ -17,92 +17,8 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-#include "../../sdk/dsp.h"
-#include "../../sdk/voice.h"
+#include "osci_voice.h"
 #include "../../sdk/common/jack_client.h"
-#include "../../sdk/common/paramui.h"
-#include <math.h>
-
-using nuclear::dsp;
-using nuclear::UI;
-
-// Include the generated file
-#include "osci.dsp.in"
-
-class Voice :
-	public nuclear::voice
-{
-public:
-	Voice() :
-		nuclear::voice(),
-		_note(0),
-		_interface(new nuclear::paramui())
-	{
-	}
-
-	int getNumInputs() { return 0; }
-	int getNumOutputs() { return 1;	}
-	void buildUserInterface(UI* interface) {}
-
-	void init(int srate)
-	{
-		_dsp.init(srate);
-		_dsp.buildUserInterface(_interface);
-		_interface->set_option("oscillator/freq", 0.000000f);
-		_interface->set_option("oscillator/volume", -96.000000f);
-	}
-
-	void compute(int nframes, float** inputs, float** outputs)
-	{
-		_dsp.compute(nframes, inputs, outputs);
-	}
-
-	void play_note(jack_midi_data_t note, jack_midi_data_t velocity)
-	{
-		_note = note;
-		_interface->set_option("oscillator/freq", note_to_frequency(_note));
-		_interface->set_option("oscillator/volume", velocity_to_amplitude(velocity));
-	}
-
-	void stop_note()
-	{
-		_note = 0;
-		_interface->set_option("oscillator/freq", 0.000000f);
-		_interface->set_option("oscillator/volume", -96.000000f);
-	}
-
-	void kill_note()
-	{
-		// Same as stop_note() since we don't have any release envelope
-		stop_note();
-	}
-
-	jack_midi_data_t note_playing()
-	{
-		return _note;
-	}
-
-private:
-	jack_default_audio_sample_t note_to_frequency(jack_midi_data_t note)
-	{
-		int n = note;
-		if (n >= 0 && n <= 119)
-			return 440.0 * pow(2.0, (n-69) / 12.0);
-		else
-			return 0.0;
-	}
-
-	jack_default_audio_sample_t velocity_to_amplitude(jack_midi_data_t velocity)
-	{
-		int v = velocity;
-		return -96.000000f + 96.000000f / 127 * v;
-	}
-
-	jack_midi_data_t _note;
-
-	mydsp _dsp;
-	nuclear::paramui* _interface;
-};
 
 class Demo4 :
 	public nuclear::jack_client
@@ -113,7 +29,7 @@ public:
 	{
 		for (int i = 0; i < 8; ++i)
 		{
-			Voice* v = new Voice();
+			osci_voice* v = new osci_voice();
 			v->init(sample_rate());
 			_voices.push_back(v);
 		}
@@ -139,12 +55,12 @@ protected:
 
 		int nactive = 0;
 		jack_default_audio_sample_t* voice_buffer = new jack_default_audio_sample_t[buffer_size()];
-		for (std::vector<Voice*>::iterator i = _voices.begin(); i != _voices.end(); ++i)
+		jack_default_audio_sample_t* output[] = { voice_buffer };
+		for (std::vector<nuclear::voice*>::iterator i = _voices.begin(); i != _voices.end(); ++i)
 		{
 			if ((*i)->note_playing() == 0) continue;
 			else nactive++;
 
-			jack_default_audio_sample_t* output[] = { voice_buffer };
 			(*i)->compute(buffer_size(), NULL, output);
 			jack_nframes_t n = buffer_size();
 			jack_default_audio_sample_t* src = voice_buffer;
@@ -162,14 +78,14 @@ protected:
 
 	void on_note_off(int port, jack_midi_data_t channel, jack_midi_data_t note, jack_midi_data_t velocity) 
 	{
-		for (std::vector<Voice*>::iterator i = _voices.begin(); i != _voices.end(); ++i)
+		for (std::vector<nuclear::voice*>::iterator i = _voices.begin(); i != _voices.end(); ++i)
 			if ((*i)->note_playing() == note)
 				(*i)->stop_note();
 	}
 
 	void on_note_on(int port, jack_midi_data_t channel, jack_midi_data_t note, jack_midi_data_t velocity) 
 	{
-		for (std::vector<Voice*>::iterator i = _voices.begin(); i != _voices.end(); ++i)
+		for (std::vector<nuclear::voice*>::iterator i = _voices.begin(); i != _voices.end(); ++i)
 		{
 			if ((*i)->note_playing() == 0)
 			{
@@ -180,8 +96,7 @@ protected:
 	}
 
 private:
-	std::vector<Voice*> _voices;
-	jack_default_audio_sample_t** _voice_buffers;
+	std::vector<nuclear::voice*> _voices;
 };
 
 
