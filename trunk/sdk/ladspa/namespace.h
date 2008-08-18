ea086238c2c4ea07a4ee6d40a5064b95ad8c1ea4
anders.dahnielson <anders.dahnielson@2299fe86-fe49-0410-b9be-31dadaebf420>
2008-08-18 21:06:46 +0000

    LADSPA header included

---
+++ trunk/sdk/ladspa/namespace.h
@@ -0,0 +1,32 @@
+#ifndef NUCLEAR_LADSPANAMESPACE_H
+#define NUCLEAR_LADSPANAMESPACE_H
+
+// Nuclear
+// Copyright (c) 2008, Anders Dahnielson
+//
+// Contact: anders@dahnielson.com
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2 of the License, or
+// (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+// Documents the nuclear::ladspa namespace
+
+namespace nuclear
+{
+	/// Namespace for LADSPA
+	namespace ladspa {}
+	
+} // !namespace nuclear
+
+#endif // !NUCLEAR_LADSPANAMESPACE_H
